59fd73a8b82852b48a3e0f4a92fa2fe245cdf84e
kenta-afk <kentarou9.60@icloud.com>
2023-12-31 18:56:10 +0900

    twice

--- manzeu.js
+++ manzeu.js
@@ -2,9 +2,11 @@ const {
     Client,
     Events,
     GatewayIntentBits
-
 } = require("discord.js");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
 
+require("dotenv").config();
 
 //clientの作成
 const client = new Client({
@@ -12,20 +14,42 @@ const client = new Client({
 });
 
 //client login
-client.login("MTE4NjYyNDM2NTY5NDAzODA2Ng.GRVETI.hxIRqu7xmar5XCT5uIEArhKS5kxQYkgofLZnbw")
+client.login(process.env.token);
 
+// 非同期の音声再生関数
+async function playAudio(message) {
+    if (message.member.voice.channel) {
+        const url = 'https://www.youtube.com/watch?v=6vJkPqIVnok'; // 固定のYouTubeのURL
+        const connection = joinVoiceChannel({
+            channelId: message.member.voice.channel.id, // メッセージを送信したユーザーのボイスチャンネルID
+            guildId: message.guild.id, // ギルドID
+            adapterCreator: message.guild.voiceAdapterCreator, // アダプタークリエーター
+        });
+        const player = createAudioPlayer();
+        const stream = ytdl(url, { filter: 'audioonly' });
+        const resource = createAudioResource(stream);
+        player.play(resource);
+        connection.subscribe(player);
+    } else {
+        message.reply('ボイスチャンネルに接続してください。');
+    }
+}
 //client event 登録
-
 client.on(Events.MessageCreate, async (message) => {
     const command = message.content;
-    if (message.auther.bot){
-        return;
-    }
+
     if (command === "うーたん") {
         await message.reply("うーうーたん現金現金やなー")
     }
-    if (command.includes("まんぜう")) {
+    if (command === "まんぜう") {
         await message.reply("んん〜まかｧｧウｯｯ!!!!🤏😎")
     }
-})
+    if (command === "コシヒカリ") {
+        await message.reply("ぽぅぽ,新潟産のコシヒカリやないかって！！")
+    }
 
+    if (command === 'まんぜうライブ') {
+        await playAudio(message);
+    }
+
+});
